6f737d07357b31aa50037496b2d828f0635ce5c7
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-24 08:38:05 -0300

    Comment ft_itoa.c

--- Additional functions/ft_itoa.c
+++ Additional functions/ft_itoa.c
@@ -2,6 +2,7 @@
 
 static int	count(long int nbr)
 {
+	//Count the number of digits 
 	unsigned int	i;
 
 	i = 1;
@@ -25,21 +26,28 @@ char	*ft_itoa(int n)
 
 	num = n;
 	num_digits = count(n);
+
+	//If it is a negative number, multiply it by -1 and add 1 to the digit counter so it's possible to allocate properly and add the negative sign at the end.
 	if (n < 0)
 	{
 		num = num * -1;
 		num_digits++;
 	}
+	// Allocates dynamically the number of digits +1 to add the'\0' in the end
 	str = (char *)malloc(sizeof(char) * (num_digits + 1));
 	if (str == NULL)
 		return (NULL);
 	*(str + num_digits) = '\0';
+
+	//While the number of digits is different from 0, fill the string backwards, taking the modulus of 10 and transforming it into char by adding '0' and then dividing the number by 10
 	while (num_digits--)
 	{
 		*(str + num_digits) = num % 10 + '0';
 		num = num / 10;
 	}
+	//If it is a negative number add the negative sign
 	if (n < 0)
 		*(str + 0) = '-';
+	//Return the string
 	return (str);
 }
