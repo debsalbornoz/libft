ef8b157204fa749e1c6ceefa059d2bb2ed33772c
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-29 20:54:13 -0400

    add comments

--- Standard functions/ft_memchr.c
+++ Standard functions/ft_memchr.c
@@ -2,16 +2,20 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*str;
+	unsigned char *str; 
 
-	str = (unsigned char *)s;
+	str = (unsigned char *)s; 
 	while (n > 0)
 	{
+		// If the current byte does not match 'c', move to the next byte in the memory area
 		if (*str != (unsigned char)c)
 			str++;
+		// If a match is found, return a pointer to the current byte
 		else
 			return ((void *)str);
 		n--;
 	}
+
+	// Return NULL if 'c' is not found in the specified memory area
 	return (NULL);
 }
