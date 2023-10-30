298c36753c262a59183eb5d45ced8b449b2575e6
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-29 20:43:51 -0400

    add comments

--- Additional functions/ft_striteri.c
+++ Additional functions/ft_striteri.c
@@ -2,11 +2,14 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	unsigned int	i;
+	unsigned int i;
 
 	i = 0;
+	
+	// Iterate through the characters of the string 's'
 	while (s[i])
 	{
+		// Apply the function 'f' to the current character and its index
 		f(i, &s[i]);
 		i++;
 	}
