7e5c88d8b4fe9f0b4ae20914bbb9e2f3e2afe3d8
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-29 20:45:01 -0400

    add comments

--- Additional functions/ft_strjoin.c
+++ Additional functions/ft_strjoin.c
@@ -2,16 +2,27 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	s3_len;
-	char	*s3;
-	char	*str1;
+	size_t s3_len;
+	char *s3;
+	char *str1;
 
+	// Cast input strings to char pointers for manipulation
 	str1 = (char *)s1;
+	
+	// Calculate the length of the resulting string
 	s3_len = ft_strlen(s1) + ft_strlen(s2);
+	
+	// Allocate memory for the concatenated string
 	s3 = (char *)malloc((s3_len + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
+	
+	// Copy the content of the first string to the concatenated string
 	ft_strlcpy(s3, str1, s3_len + 1);
+	
+	// Concatenate the second string to the concatenated string
 	ft_strlcat(s3, s2, s3_len + 1);
+	
+	// Return the concatenated string
 	return (s3);
 }
