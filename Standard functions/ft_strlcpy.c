f2b1c9c26a439a654ea80e9ac9bd7e24830f1cc6
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-29 21:09:15 -0400

    add comments

--- Standard functions/ft_strlcpy.c
+++ Standard functions/ft_strlcpy.c
@@ -2,15 +2,20 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	src_len;
+	size_t i;
+	size_t src_len;
 
 	i = 0;
-	src_len = ft_strlen(src);
+	src_len = ft_strlen(src);  // Calculate the length of the source string
+
+	// Handle cases where destination or source pointers are NULL
 	if (dst == NULL || src == NULL)
 		return (0);
+	// If size is 0, return the length of the source string
 	else if (size == 0)
 		return (src_len);
+
+	// Copy characters from src to dst until size - 1 characters are copied or src ends
 	while (i < size - 1 && *src != '\0')
 	{
 		*dst = *src;
@@ -18,6 +23,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 		dst++;
 		i++;
 	}
-	*dst = '\0';
+	*dst = '\0';  // Null-terminate the destination string
+
+	// Return the total length of the source string
 	return (src_len);
 }
