68a40b6819eb063db392ea13d89dfdb3eb0375d6
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-29 21:05:35 -0400

    add comments

--- Standard functions/ft_strdup.c
+++ Standard functions/ft_strdup.c
@@ -2,21 +2,26 @@
 
 char	*ft_strdup(const char *s)
 {
-	int		len;
-	char	*duplicate;
-	int		i;
+	int len;
+	char *duplicate;
+	int i;
 
-	len = ft_strlen(s) + 1;
-	duplicate = (char *)malloc(len * sizeof(char));
+	len = ft_strlen(s) + 1;  // Calculate the length of the input string, including the null terminator
+	duplicate = (char *)malloc(len * sizeof(char));  // Allocate memory for the duplicate string
 	if (duplicate == NULL)
-		return (NULL);
-	*duplicate = '\0';
+		return (NULL);  // Return NULL if memory allocation fails
+
+	*duplicate = '\0'; 
 	i = 0;
+
+	// Copy characters from the input string to the duplicate string
 	while (s[i] != '\0')
 	{
 		duplicate[i] = s[i];
 		i++;
 	}
-	duplicate[i] = '\0';
-	return (duplicate);
+	duplicate[i] = '\0';  // Add null terminator at the end of the duplicate string
+
+	return (duplicate);  // Return the newly allocated duplicate string
 }
+
