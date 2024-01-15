c27189d86b6cdab1baa2de5809ca8c2d68b32803
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2024-01-15 18:01:52 +0000

    add comments

--- ft_bzero.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-/*This function sets a block of memory, specified by the pointer s and the size n, to zero.
-Essentially, it clears the bytes in the memory block, making it null. 
-*/
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, 0, n);
-}
--- ft_putendl_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-//Writes the string s followed by a newline character to the specified file descriptor 
-
-void	ft_putendl_fd(char *s, int fd)
-{
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
-}
